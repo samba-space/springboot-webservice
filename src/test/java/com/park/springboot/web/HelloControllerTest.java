2a834403cefcd042fc86174608e90011bc0f32b2
Park <junkipark430@gmail.com>
2021-03-27 01:00:23 +0900

    미사용 import 제거

--- src/test/java/com/park/springboot/web/HelloControllerTest.java
+++ src/test/java/com/park/springboot/web/HelloControllerTest.java
@@ -2,7 +2,6 @@ package com.park.springboot.web;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.mockito.Mock;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.test.context.junit4.SpringRunner;
