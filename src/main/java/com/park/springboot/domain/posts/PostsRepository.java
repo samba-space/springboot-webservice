fb70c4357e857d77b7e4cb7458ab240eaa6bd1c7
Park <junkipark430@gmail.com>
2021-03-29 20:59:26 +0900

    Jpa 적용

---
+++ src/main/java/com/park/springboot/domain/posts/PostsRepository.java
@@ -0,0 +1,7 @@
+package com.park.springboot.domain.posts;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface PostsRepository extends JpaRepository<Posts, Long> {
+
+}
