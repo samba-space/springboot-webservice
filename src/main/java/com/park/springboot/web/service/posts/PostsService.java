7c81a1e444a75cf699ab866e6e437c8875ddf6e2
Park <junkipark430@gmail.com>
2021-03-30 19:43:02 +0900

    게시글 조회, 수정 api 작성

--- src/main/java/com/park/springboot/web/service/posts/PostsService.java
+++ src/main/java/com/park/springboot/web/service/posts/PostsService.java
@@ -1,7 +1,10 @@
 package com.park.springboot.web.service.posts;
 
+import com.park.springboot.domain.posts.Posts;
 import com.park.springboot.domain.posts.PostsRepository;
+import com.park.springboot.web.dto.PostsResponseDto;
 import com.park.springboot.web.dto.PostsSaveRequestDto;
+import com.park.springboot.web.dto.PostsUpdateRequestDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -14,5 +17,19 @@ public class PostsService {
     @Transactional
     public Long save(PostsSaveRequestDto requestDto){
         return postsRepository.save(requestDto.toEntity()).getId();
+    }
+
+    @Transactional
+    public Long update(Long id, PostsUpdateRequestDto requestDto){
+//        Posts posts =
+
+        return null;
+    }
+
+    public PostsResponseDto findById(Long id){
+        Posts entity = postsRepository.findById(id)
+                .orElseThrow(() -> new IllegalArgumentException("해당 사용자가 없습니다. id = " + id));
+
+        return new PostsResponseDto(entity);
     }
 }
