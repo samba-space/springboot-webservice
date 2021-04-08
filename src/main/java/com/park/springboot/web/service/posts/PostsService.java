e86855e7ee1727acab472a08865b253a5211b7b3
Park <junkipark430@gmail.com>
2021-04-08 23:37:19 +0900

    post service 주석 작성

--- src/main/java/com/park/springboot/web/service/posts/PostsService.java
+++ src/main/java/com/park/springboot/web/service/posts/PostsService.java
@@ -21,11 +21,22 @@ import java.util.stream.Collectors;
 public class PostsService {
     private final PostsRepository postsRepository;
 
+    /**
+     * 게시물 저장
+     * @param requestDto
+     * @return
+     */
     @Transactional
     public Long save(PostsSaveRequestDto requestDto){
         return postsRepository.save(requestDto.toEntity()).getId();
     }
 
+    /**
+     * 게시물 수
+     * @param id
+     * @param requestDto
+     * @return
+     */
     @Transactional
     public Long update(Long id, PostsUpdateRequestDto requestDto){
         Posts posts = postsRepository.findById(id)
@@ -36,6 +47,11 @@ public class PostsService {
         return id;
     }
 
+    /**
+     * 게시물 조회
+     * @param id
+     * @return
+     */
     public PostsResponseDto findById(Long id){
         Posts entity = postsRepository.findById(id)
                 .orElseThrow(() -> new IllegalArgumentException("해당 사용자가 없습니다. id = " + id));
@@ -43,12 +59,21 @@ public class PostsService {
         return new PostsResponseDto(entity);
     }
 
+    /**
+     * 게시물 목록 조회
+     * @return
+     */
     @Transactional(readOnly = true)
     public List<PostsListResponseDto> findAllDesc(){
         return postsRepository.findAllDesc().stream()
                 .map(PostsListResponseDto::new)
                 .collect(Collectors.toList());
     }
+
+    /**
+     * 게시물 삭제
+     * @param id
+     */
     @Transactional
     public void delete(Long id){
         Posts posts = postsRepository.findById(id)
