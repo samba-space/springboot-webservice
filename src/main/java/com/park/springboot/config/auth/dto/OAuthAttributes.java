9fa2d0a7828bdaa51c9d6e683e5a26167f8c658f
Park <junkipark430@gmail.com>
2021-04-12 17:41:05 +0900

    생성자 builder에서 class 레벨로 변경

--- src/main/java/com/park/springboot/config/auth/dto/OAuthAttributes.java
+++ src/main/java/com/park/springboot/config/auth/dto/OAuthAttributes.java
@@ -7,6 +7,7 @@ import lombok.Getter;
 
 import java.util.Map;
 
+@Builder
 @Getter
 public class OAuthAttributes {
     private Map<String, Object> attributes;
@@ -14,15 +15,6 @@ public class OAuthAttributes {
     private String name;
     private String email;
     private String picture;
-
-    @Builder
-    public OAuthAttributes(Map<String, Object> attributes, String nameAttributeKey, String name, String email, String picture){
-        this.attributes = attributes;
-        this.nameAttributeKey = nameAttributeKey;
-        this.name = name;
-        this.email = email;
-        this.picture = picture;
-    }
 
     public static OAuthAttributes of(String registrationId, String userNameAttributeName, Map<String,Object> attributes){
         return ofGoogle(userNameAttributeName, attributes);
