2897e48f14c22d4cd1a8849bdc87753fb277a28a
Park <junkipark430@gmail.com>
2021-04-18 23:32:48 +0900

    naver 로그인 추가

--- src/main/java/com/park/springboot/config/auth/dto/OAuthAttributes.java
+++ src/main/java/com/park/springboot/config/auth/dto/OAuthAttributes.java
@@ -17,6 +17,10 @@ public class OAuthAttributes {
     private String picture;
 
     public static OAuthAttributes of(String registrationId, String userNameAttributeName, Map<String,Object> attributes){
+        if("naver".equals(registrationId)){
+            return ofNaver("id", attributes);
+        }
+
         return ofGoogle(userNameAttributeName, attributes);
     }
 
@@ -26,6 +30,18 @@ public class OAuthAttributes {
                 .email((String)attributes.get("email"))
                 .picture((String)attributes.get("picture"))
                 .attributes(attributes)
+                .nameAttributeKey(userNameAttributeName)
+                .build();
+    }
+
+    private static OAuthAttributes ofNaver(String userNameAttributeName, Map<String, Object> attributes){
+        Map<String, Object> response = (Map<String, Object>) attributes.get("response");
+
+        return OAuthAttributes.builder()
+                .name((String)response.get("name"))
+                .email((String)response.get("email"))
+                .picture((String)response.get("profileImage"))
+                .attributes(response)
                 .nameAttributeKey(userNameAttributeName)
                 .build();
     }
